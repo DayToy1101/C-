eb90e63a805aa80d73b0be0189dbc10d24b76f12
DT <2573263498@qq.com>
2019-11-19 14:02:52 +0800

    20191119

---
+++ 20191119/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20191119
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text==string.Empty||textBox2.Text==string.Empty)
+            {
+                MessageBox.Show("输入不完整！");
+                return;
+            }
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            int c = a + b;
+            textBox3.Text = c.ToString();
+            //隐藏窗体   this.Hide()也是隐藏窗体的
+            //this.Visible = false;
+        }
+    }
+}
