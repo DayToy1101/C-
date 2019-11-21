049467a07702af12b471ebb4b08a5f7f1b46ec17
DT <2573263498@qq.com>
2019-11-21 17:16:59 +0800

    20191121

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double sum = 0.5, t, t1, t2, t3, p = 0.5 * 0.5;
+            int odd = 1, even = 2;
+            t=t1=t2=1.0;
+            t3=0.5;
+            while (t > 1e-10)
+            {
+                t1 = t1 * odd / even;
+                odd += 2;
+                even += 2;
+                t2 = 1.0 / odd;
+                t3 = t3 * p;
+                t = t1 * t2 * t3;
+                sum += t;
+            }
+            Console.WriteLine("\nPI={0,10:f8}", sum * 6);
+            Console.Read();
+        }
+    }
+}
