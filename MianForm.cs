713c40ed9c4a35086a22b1c752ef15d1af498cf9
DT <2573263498@qq.com>
2019-11-29 20:58:43 +0800

    20191129

---
+++ MianForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 多文档界面
+{
+    public partial class MianForm : Form
+    {
+        public MianForm()
+        {
+            InitializeComponent();
+        }
+
+        private void 新建NToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm MDIChild = new ChildForm();
+            MDIChild.MdiParent = this;
+            MDIChild.Show();
+        }
+    }
+}
