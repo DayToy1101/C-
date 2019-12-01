c0e2a5e2b638cff5779871f042dbf7df61fa41ad
a <a>
2019-12-01 17:43:22 +0800

    20191201

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            float y = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                float aa = 1;
+                for (int j = 1; j <= i; j++)
+                {
+                    aa = j * aa;
+                }
+                y += aa;
+            }
+            Console.WriteLine(n.ToString());
+            Console.WriteLine(y.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
