a4e3270b8658b07084883440575f5d38bd6ed47d
a <2573263498@qq.com>
2019-12-02 20:38:06 +0800

    20191202

---
+++ 2_5_3_/2_5_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("三位数中：");
+            for (int i = 100; i <= 999; ++i)
+            {
+                if (num(i) == true)
+                {
+                    Console.Write("{0}是水仙花数   ", i);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            Console.ReadKey(false);
+        }
+        static bool num(int number)
+        {
+            int d3 = number / 100;
+            int d2 = number % 100 / 10;
+            int d1 = number % 100 % 10;
+            int N = d3 * d3 * d3 + d2 * d2 * d2 + d1 * d1 * d1;
+            if (number == N)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
