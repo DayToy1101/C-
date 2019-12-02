d30f1b883252d2be1a19c2a141b7de1e51e8c548
a <2573263498@qq.com>
2019-12-02 20:39:48 +0800

    20191202

---
+++ 2_5_4_/2_5_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个整数");
+            string word = Console.ReadLine();
+            for (int i = word.Length - 1; i > -1; i--)
+            {
+                Console.Write(word[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
