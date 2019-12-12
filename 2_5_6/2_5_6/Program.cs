d14bcdfc9d0c395833235dc1eb4b298885dc37a8
a <2573263498@qq.com>
2019-12-12 20:08:09 +0800

    20191212

---
+++ 2_5_6/2_5_6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_5_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个整数：");
+            int x = Convert.ToInt32(Console.ReadLine());
+            //注意得到的只是单个表示
+            string t = Convert.ToString(x,16);
+            //输出最后结果
+            Console.WriteLine(t);
+            Console.ReadKey();
+        }
+    }
+}
