d1f067d61bca80d6acade1fdfbd32fa8a7a7b7e7
a <2573263498@qq.com>
2020-03-30 16:39:20 +0800

    20200330

---
+++ Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTest
+{
+    class Manager
+    {
+         public Manager()
+        {
+            Console.WriteLine("创建经理对象");
+        }
+        public void deductSalary(object sender,EventArgs e)
+        {
+            Console.WriteLine("好小子，你敢上班玩游戏，扣你工资！");
+            ((Employee)sender).Salary -= 100;
+        }
+    }
+}
