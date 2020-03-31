ce4d7c44f11fa186819512a044273f7c41678ed0
a <2573263498@qq.com>
2020-03-31 11:10:12 +0800

    20200331

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("<h1>欢迎进入我的主页</h1>");
+        }
+    }
+}
