df06c18f376cd9bde560c6f696f5466262d6df18
a <2573263498@qq.com>
2020-03-31 11:09:17 +0800

    20200331

---
+++ Manager_ori.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTest
+{
+    class Manager_ori
+    {
+        public Manager_ori()
+        {
+            Console.WriteLine("创建经理对象");
+        }
+        public void deductSalary(Employee_ori emploryee)
+        {
+            Console.WriteLine("你小子敢上班玩游戏，扣工资！");
+            emploryee.Salary -= 100;
+        }
+    }
+}
